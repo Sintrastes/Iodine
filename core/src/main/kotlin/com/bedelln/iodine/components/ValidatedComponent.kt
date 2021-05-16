cfcea5ee419002989048c022fd8ba32b8fecff69
Nathan Bedell <nbedell@tulane.edu>
2021-05-16 15:03:05 -0400

    Added validated component and description.

--- core/src/main/kotlin/com/bedelln/iodine/components/ValidatedComponent.kt
+++ core/src/main/kotlin/com/bedelln/iodine/components/ValidatedComponent.kt
@@ -2,24 +2,61 @@ package com.bedelln.iodine.components
 
 import androidx.compose.runtime.Composable
 import arrow.core.Either
-import com.bedelln.iodine.HComponent
-import com.bedelln.iodine.HComponentDescription
-import com.bedelln.iodine.IodineContext
+import com.bedelln.iodine.interfaces.HComponent
+import com.bedelln.iodine.interfaces.HComponentDescription
+import com.bedelln.iodine.interfaces.IodineContext
+import com.bedelln.iodine.interfaces.SettableHComponentDescription
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 sealed class ValidationEvent<in E> {
     class SubmitForValidation<E>(): ValidationEvent<E>()
     data class Other<E>(val event: E): ValidationEvent<E>()
 }
 
+/** Helper class for building components with validation. */
 class ValidatedComponent<C: IodineContext, Ei, Eo, A, B, Err>(
-    contents: HComponentDescription<C, Ei, Eo, Pair<Err?,A>, Either<B, Err>>
-): HComponentDescription<C, ValidationEvent<Ei>, Eo, A, B> {
+    val contents: SettableHComponentDescription<C, Ei, Eo, Pair<Err?, A>, Either<Pair<Err, A>, B>>
+): HComponentDescription<C, ValidationEvent<Ei>, Eo, A, B?> {
     @Composable
     override fun initCompose(ctx: C) {
-        TODO("Not yet implemented")
+        contents.initCompose(ctx)
     }
 
-    override fun initialize(ctx: C, initialValue: A): HComponent<ValidationEvent<Ei>, Eo, A, B> {
-        TODO("Not yet implemented")
+    override fun initialize(ctx: C, initialValue: A): HComponent<ValidationEvent<Ei>, Eo, A, B?> {
+        return object: HComponent<ValidationEvent<Ei>, Eo, A, B?> {
+
+            val component = contents.initialize(ctx, Pair(null, initialValue))
+            val resultFlow = MutableStateFlow(
+                component.result.value.orNull()
+            )
+
+            @Composable
+            override fun contents() {
+                component.contents()
+            }
+
+            override fun onEvent(event: ValidationEvent<Ei>) = with(component) {
+                if(event is ValidationEvent.SubmitForValidation) {
+                    when(val result = result.value) {
+                        is Either.Left -> {
+                            ctx.setValue(result.value)
+                            ctx.defaultScope.launch {
+                                resultFlow.emit(
+                                        component.result.value.orNull()
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+
+            override val events: Flow<Eo>
+                get() = component.events
+            override val result: StateFlow<B?>
+                get() = resultFlow
+        }
     }
 }
--- core/src/main/kotlin/com/bedelln/iodine/components/text/IntEntry.kt
+++ core/src/main/kotlin/com/bedelln/iodine/components/text/IntEntry.kt
@@ -1,15 +1,92 @@
 package com.bedelln.iodine.components.text
 
-import androidx.compose.runtime.Composable
-import com.bedelln.iodine.*
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import arrow.core.Either
+import com.bedelln.iodine.components.ValidatedComponent
+import com.bedelln.iodine.components.ValidationEvent
+import com.bedelln.iodine.interfaces.HComponentDescription
+import com.bedelln.iodine.interfaces.IodineContext
+import com.bedelln.iodine.interfaces.SettableHComponent
+import com.bedelln.iodine.interfaces.SettableHComponentDescription
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.SharingStarted.Companion.Lazily
+import kotlinx.coroutines.launch
 
-class IntEntry<C: IodineContext>(): HComponentDescription<C, Void, Void, Int, Int> {
-    @Composable
-    override fun initCompose(ctx: C) {
-        TODO("Not yet implemented")
-    }
+object InvalidInteger
 
-    override fun initialize(ctx: C, initialValue: Int): HComponent<Void, Void, Int, Int> {
-        TODO("Not yet implemented")
-    }
-}
+/** A component for inputting integers. */
+class IntEntry<C : IodineContext> : HComponentDescription<C, ValidationEvent<Void>, Void, String, Int?> by (
+    ValidatedComponent<C, Void, Void, String, Int, InvalidInteger>(
+        object :
+            SettableHComponentDescription<
+                C, Void, Void,
+                Pair<InvalidInteger?, String>,
+                Either<Pair<InvalidInteger, String>, Int>
+                > {
+            @Composable
+            override fun initCompose(ctx: C) { }
+
+            override fun initialize(ctx: C, initialValue: Pair<InvalidInteger?, String>) =
+                object : SettableHComponent<C, Void, Void, Pair<InvalidInteger?, String>, Either<Pair<InvalidInteger, String>, Int>> {
+
+                    val showingError = MutableStateFlow(
+                        initialValue.first != null
+                    )
+                    val textContents = MutableStateFlow(
+                        initialValue.second
+                    )
+
+                    override fun C.setValue(newValue: Pair<InvalidInteger?, String>) {
+                        TODO("Not yet implemented")
+                    }
+
+                    @Composable
+                    override fun contents() {
+                        val errorState = showingError.collectAsState()
+                        val contentsState = textContents.collectAsState()
+                        val showingError by remember { errorState }
+                        val contents by remember { contentsState }
+                        TextField(
+                            value = contents,
+                            onValueChange = { newValue ->
+                                ctx.defaultScope.launch {
+                                    textContents.emit(newValue)
+                                }
+                            },
+                            label = {
+                                Text("")
+                            }
+                        )
+                        if (showingError) {
+                            Text(
+                                text = "Not a valid integer",
+                                color = Color.Red
+                            )
+                        }
+                    }
+
+                    fun getResults(err: Boolean, contents: String) =
+                        if(err) {
+                            Either.Left(Pair(InvalidInteger, contents))
+                        } else {
+                            Either.Right(contents.toInt())
+                        }
+
+                    override fun onEvent(event: Void) { }
+                    override val events: Flow<Void>
+                        get() = emptyFlow()
+                    override val result: StateFlow<Either<Pair<InvalidInteger, String>, Int>>
+                        get() = showingError.combine(textContents) { err, contents ->
+                            getResults(err, contents)
+                        }.stateIn(
+                            ctx.defaultScope,
+                            Lazily,
+                            getResults(showingError.value, textContents.value)
+                        )
+                }
+        }
+    )
+)
